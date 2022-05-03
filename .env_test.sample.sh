79b94b8427850777b2605a75e334f8b81a68fabb
Mike Cunningham <mikeecunningham@hotmail.com>
2022-05-03 00:38:46 -0400

    env shell files

---
+++ .env_test.sample.sh
@@ -0,0 +1,16 @@
+export ENV=TEST
+export BYBIT_KEY=
+export BYBIT_SECRET=
+export BYBIT_PERPETUALS_URL=wss://stream-testnet.bybit.com/realtime_public
+export BYBIT_PERPETUALS_PRIVATE_URL=wss://stream-testnet.bybit.com/realtime_private
+export BYBIT_REST_URL=https://api-testnet.bybit.com
+export PHEMEX_KEY=
+export PHEMEX_SECRET=
+export PHEMEX_PERPETUALS_URL=
+export PHEMEX_PERPETUALS_PRIVATE_URL=
+export PHEMEX_REST_URL=
+export BINANCE_KEY=
+export BINANCE_SECRET=
+export BINANCE_PERPETUALS_URL=wss://stream.binancefuture.com
+export BINANCE_REST_URL=https://testnet.binancefuture.com
+export RUST_BACKTRACE=1
